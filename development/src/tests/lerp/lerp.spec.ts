70947d214cebe5bd0978b5ab926ee8190d85da2a
Lee Winder <lee.winder@gmail.com>
2016-08-25 14:25:09 +0100

    Extracted the overtime and at 1 tests into a helper function to reduce code

--- development/src/tests/lerp/lerp.spec.ts
+++ development/src/tests/lerp/lerp.spec.ts
@@ -27,24 +27,14 @@ describe('Lerp Tests', () => {
         this.lerp.define([[0, 1]], 10, Transition.EaseOut, Style.Quadratic);
     });
 
-    it('Lerp completes after given duration', function () {
-        this.lerp.define([[0, 1], [3, 6]], 10);
+    it('Lerp completes after given duration - EaseOut Quadratic', function () {
+        this.lerp.define([[0, 1], [3, 6]], 10, Transition.EaseOut, Style.Quadratic);
+        checkLerpCompletesAtOne(this.lerp, 1, 6, 10);
+    });
 
-        let lastResults: number[] = [0, 0];
-        let lastTime = 0;
-        this.lerp.lerp((results: number[], time: number) => {
-            // Just save our values
-            lastResults = results;
-            lastTime = time;
-        });
-
-        // Go forward
-        jasmine.clock().tick(1000 * 10);
-
-        // Check we finished
-        expect(lastResults[0]).toBe(1);
-        expect(lastResults[1]).toBe(6);
-        expect(lastTime).toBe(1);
+    it('Lerp completes after given duration - EaseOut Linear', function () {
+        this.lerp.define([[0, 1], [3, 6]], 10, Transition.EaseOut, Style.Linear);
+        checkLerpCompletesAtOne(this.lerp, 1, 6, 10);
     });
 
     it('Lerp reports time in a linear manner', function () {
@@ -67,84 +57,14 @@ describe('Lerp Tests', () => {
     it('Lerp increases in value - Transition.EaseOut, Style.Quadratic', function (done) {
 
         this.lerp.define([[0, 1], [3, 5]], 10, Transition.EaseOut, Style.Quadratic);
-
-        // Uninstall the clock so our setTimeout works correctly
-        // as mocking the clock doesn't really trigger our intervals
-        jasmine.clock().uninstall();
-
-        // We need to track multiple values
-        let firstResult: number[] = null;
-        let secondResult: number[] = null;
-
-        this.lerp.lerp((results: number[], time: number) => {
-
-            if (firstResult === null) {
-                firstResult = results;
-            } else if (secondResult == null) {
-                secondResult = results;
-            }
-        });
-
-        // Wait for our work to be done
-        setTimeout(() => {
-
-            // Expect our increased values to be around the same each time
-            expect(firstResult[0]).toBeCloseTo(0.00718704, 1);
-            expect(firstResult[1]).toBeCloseTo(3.01437408, 1);
-
-            expect(secondResult[0]).toBeCloseTo(0.0159359, 1);
-            expect(secondResult[1]).toBeCloseTo(3.031872, 1);
-
-            expect(secondResult[0]).toBeGreaterThan(firstResult[0]);
-            expect(secondResult[1]).toBeGreaterThan(firstResult[1]);
-
-            // Finish this test
-            done();
-
-        }, 300);
+        checkLerpOverTime(done, this.lerp, 0.00718704, 0.0159359, 1, 3.01437408, 3.031872, 1);
     });
 
     // Hard to indicate what these values _should_ be, so hard coded
     it('Lerp increases in value - Transition.EaseOut, Style.Linear', function (done) {
 
         this.lerp.define([[0, 1], [3, 5]], 4, Transition.EaseOut, Style.Linear);
-
-        // Uninstall the clock so our setTimeout works correctly
-        // as mocking the clock doesn't really trigger our intervals
-        jasmine.clock().uninstall();
-
-        // We need to track multiple values
-        let firstResult: number[] = null;
-        let secondResult: number[] = null ;
-
-        this.lerp.lerp((results: number[], time: number) => {
-
-            if (firstResult === null) {
-                firstResult = results;
-            } else if (secondResult == null) {
-                secondResult = results;
-            }
-
-            console.log(results[1]);
-        });
-
-        // Wait for our work to be done
-        setTimeout(() => {
-
-            // Expect our increased values to be around the same each time
-            expect(firstResult[0]).toBeCloseTo(0.00925, 2);
-            expect(firstResult[1]).toBeCloseTo(3.020, 2);
-
-            expect(secondResult[0]).toBeCloseTo(0.017, 2);
-            expect(secondResult[1]).toBeCloseTo(3.033, 2);
-
-            expect(secondResult[0]).toBeGreaterThan(firstResult[0]);
-            expect(secondResult[1]).toBeGreaterThan(firstResult[1]);
-
-            // Finish this test
-            done();
-
-        }, 100);
+        checkLerpOverTime(done, this.lerp, 0.00925, 0.017, 2, 3.020, 3.033, 2);
     });
 
     // Hard to indicate what these values _should_ be, so hard coded
@@ -262,4 +182,71 @@ describe('Lerp Tests', () => {
         }, 500);
     });
 
+
+    // Support Functions
+
+    //
+    // Checks for a given lerp that after the run, the lerp is complete, time is at 1 and we're at the end values
+    //
+    function checkLerpCompletesAtOne(lerp: Lerp, rangeOne: number, rangeTwo: number, duration: number) {
+
+        let lastResults: number[] = [0, 0];
+        let lastTime = 0;
+        lerp.lerp((results: number[], time: number) => {
+            // Just save our values
+            lastResults = results;
+            lastTime = time;
+        });
+
+        // Go forward
+        jasmine.clock().tick(1000 * duration);
+
+        // Check we finished
+        expect(lastResults[0]).toBe(rangeOne);
+        expect(lastResults[1]).toBe(rangeTwo);
+        expect(lastTime).toBe(1);
+    }
+
+    //
+    // For a given lerp, tracks the expected value over time
+    //
+    function checkLerpOverTime(done: DoneFn, lerp: Lerp,
+                               set1First: number, set1Second: number, set1Accuracy: number,
+                               set2First: number, set2Second: number, set2Accuracy: number) {
+
+        // Uninstall the clock so our setTimeout works correctly
+        // as mocking the clock doesn't really trigger our intervals
+        jasmine.clock().uninstall();
+
+        // We need to track multiple values
+        let firstResult: number[] = null;
+        let secondResult: number[] = null;
+
+        lerp.lerp((results: number[], time: number) => {
+
+            if (firstResult === null) {
+                firstResult = results;
+            } else if (secondResult == null) {
+                secondResult = results;
+            }
+        });
+
+        // Wait for our work to be done
+        setTimeout(() => {
+
+            // Expect our increased values to be around the same each time
+            expect(firstResult[0]).toBeCloseTo(set1First, set1Accuracy);
+            expect(firstResult[1]).toBeCloseTo(set2First, set2Accuracy);
+
+            expect(secondResult[0]).toBeCloseTo(set1Second, set1Accuracy);
+            expect(secondResult[1]).toBeCloseTo(set2Second, set2Accuracy);
+
+            expect(secondResult[0]).toBeGreaterThan(firstResult[0]);
+            expect(secondResult[1]).toBeGreaterThan(firstResult[1]);
+
+            // Finish this test
+            done();
+
+        }, 100);
+    }
 });
