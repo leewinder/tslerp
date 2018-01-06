5a3a4aee6f94c8d139829f5111cd07f97266263a
Lee Winder <lee.winder@gmail.com>
2018-01-06 22:06:16 +0000

    Updated tsconfig to build correctly against @types

--- development/src/lib/index.ts
+++ development/src/lib/index.ts
@@ -1,4 +1,3 @@
 export { Lerp as TsLerp }                   from './lerp/lerp';
 export { Transition as TsLerpTransition }   from './lerp/lerp';
 export { Style as TsLerpStyle }             from './lerp/lerp';
-
